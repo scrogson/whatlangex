3c3c26abef639c1dcc2f8305700190ce1ce2204c
Sonny Scroggin <sonny@scrogg.in>
2024-05-08 12:50:21 -0500

    add checksum file

---
+++ checksum-Elixir.Whatlangex.Native.exs
@@ -0,0 +1,11 @@
+%{
+  "libwhatlang_nif-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:e05dac527445c1ca2b0016b75db5c2c33ea941ca2f095a1167c4c4a4ba338c53",
+  "libwhatlang_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9e8b83027e8f25796e7225cb4772d0c211fd6a456e2be26ee2c4174a25b7dc22",
+  "libwhatlang_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:b3921f7cc869a620439ffbded5857b354dae7b0cc32d65241ca2fe3bb685a4c6",
+  "libwhatlang_nif-v0.3.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:1aa10437714d8ef3303abdf10cf8856ce876ff2f4b044fe599ef940dd95df973",
+  "libwhatlang_nif-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:209f14c70099b0dc80ed4308a849f7d079bdf3b782cab01b322cdfb783d8b544",
+  "libwhatlang_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:64c9c8d8c64c8d3c12b1852dc0651fbf02be45ff23e361f241b80e25b2a27616",
+  "libwhatlang_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:fd0180047a89ae60f3e3380ff4e31a936c6deff18c92a50c6888b713fcd1623b",
+  "whatlang_nif-v0.3.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:9841b1ac231c0343cebec36b6eaf6a760edae6303b8dfdf0fe90a670bfb4af0f",
+  "whatlang_nif-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:6efb0c1ca3d018b8e67a739c1adc5c6191db073c61d88d09c41de5e3bcfec216",
+}
